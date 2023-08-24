bbb91859c7f923b8960a496342a275e894992318
Lucas Moreira <lucasdanmoreira@gmail.com>
2023-08-24 14:44:26 +0100

    Users improvements

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get  'login',  to: 'sessions#new'
+  get  'logout', to: 'sessions#destroy'
+  post 'login',  to: 'sessions#create'
+
   get  'signup', to: 'users#new'
   post 'signup', to: 'users#create'
 
